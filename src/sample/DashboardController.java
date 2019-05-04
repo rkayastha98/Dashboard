42f2eb1c9294520680f9f2d7118dc5a3441eeb42
rkayastha98 <rkayastha@wpi.edu>
2019-05-04 00:51:40 -0400

    dashboard

--- src/sample/DashboardController.java
+++ src/sample/DashboardController.java
@@ -1,5 +1,39 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.TilePane;
+
 public class DashboardController {
 
+    @FXML
+    public ImageView logOutBtn;
+    @FXML
+    public Label tipsPane;
+    @FXML
+    public AnchorPane weather;
+    @FXML
+    public AnchorPane calendarBtn;
+    @FXML
+    public AnchorPane travelBtn;
+    @FXML
+    public AnchorPane budgetBtn;
+    @FXML
+    public AnchorPane fitnessBtn;
+    @FXML
+    public AnchorPane diaryBtn;
+    @FXML
+    public AnchorPane listBtn;
+    @FXML
+    public TilePane tasksList;
+    @FXML
+    public ImageView searchBtn;
+    @FXML
+    public ImageView aboutBtn;
+    @FXML
+    public ImageView helpBtn;
+    @FXML
+    public ImageView settingsBtn;
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/*different selectable themes with different icons, buttons changing colors on double tap, different languages, weather for today and time (weather for every hour on a pane that slides open when tapped on the weather pane, different organizational tips, reminders appearing, maybe voice recognition, mobile app*/
+/*purple: #d071e8, red: #ed2a1c, blue: #1166c6, yellow: #FFFF00, light blue: #1dc1c6, orange: #ff7200, pink: #f4027b; black;grey: #cecccc, different blue: #75CDD8*/
 public class Main extends Application {
 
     @Override
