82410e29916fe58f1b1f248b037ec5401fcc9811
rkayastha98 <rkayastha@wpi.edu>
2019-05-04 22:52:54 -0400

    dashboard
    login screen

---
+++ src/sample/mainScreenController.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class mainScreenController {
+    @FXML
+    public ImageView loginBtn;
+    @FXML
+    public AnchorPane weatherBox;
+    @FXML
+    public AnchorPane weather;
+}
